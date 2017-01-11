73ae46e7b84d707b70b59c81bfd7bc665cb1b9d6
Andy Watkins <amw579@nyu.edu>
2017-01-10 18:05:06 -0800

    Moved *py files to dirs

--- base_pair.py
+++
@@ -1,8 +0,0 @@
-from nucleotide import Nucleotide
-
-class BasePair:
-	def __init__( self, nt1, nt2 ):
-		self.nt1 = nt1
-		self.nt2 = nt2
-		self.nt1.bp_partner = nt2
-		self.nt2.bp_partner = nt1
--- nucleotide.py
+++
@@ -1,8 +0,0 @@
-class Nucleotide:
-	def __init__( self, one_letter_code, seqpos ):
-		self.name = one_letter_code
-		self.seqpos = seqpos
-		self.x = 0
-		self.y = 0
-		self.bp_partner = None
-		
