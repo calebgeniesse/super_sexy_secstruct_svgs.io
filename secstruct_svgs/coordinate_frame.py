1dd13ecc408c5f1a052d9eee08cbfdbdd8e97db4
Andy Watkins <amw579@nyu.edu>
2017-01-10 18:04:34 -0800

    Module dir

---
+++ secstruct_svgs/coordinate_frame.py
@@ -0,0 +1,16 @@
+
+
+class CoordinateFrame:
+	def __init__( self, pos, orientation, pos2=None ):
+		"""
+		Note pos2 is default None (stems don't have a terminus... yet?)
+		"""
+		self.position = Position( pos[0], pos[1] )
+		self.orientation = orientation
+		if pos2 is not None:
+			self.position2 = Position( pos2[0], pos2[1] )
+		
+class Position:
+	def __init__( self, x, y ):
+		self.x = x
+		self.y = y
